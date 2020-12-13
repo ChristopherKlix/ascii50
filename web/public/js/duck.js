6d9e7016f4066a6f9d579431537950f242d9ce85
ChristopherKlix <70777673+ChristopherKlix@users.noreply.github.com>
2020-12-13 18:47:33 +0100

    Update

--- web/public/js/duck.js
+++ web/public/js/duck.js
@@ -2,27 +2,27 @@
 
 // get reference to ddb
 let duck = document.getElementById("duck");
-// get reference to quark-put -- like output but for ducks
-let quark_put = document.getElementById("quark-put");
+// get reference to quack-put -- like output but for ducks
+let quack_put = document.getElementById("quack-put");
 
-// add event listener to ddb to quark random ASCII when clicked
-duck.addEventListener('click', quark);
+// add event listener to ddb to quack random ASCII when clicked
+duck.addEventListener('click', quack);
 
-// actual quark function to quark random ASCII when clicked
-function quark() {
+// actual quack function to quack random ASCII when clicked
+function quack() {
     // 49 to 126
     // Math.random() returns value between 0 - 1
     let char = (Math.random() * 77 + 49);
 
-    // set quark-put to random char
-    quark_put.innerHTML = String.fromCharCode(parseInt(char, 10));
+    // set quack-put to random char
+    quack_put.innerHTML = String.fromCharCode(parseInt(char, 10));
 }
 
 function matrix() {
-    quark_put.innerHTML = '&#x1F407';
+    quack_put.innerHTML = '&#x1F407';
 }
 
-// Triggers 'copied' next to quark-put
+// Triggers 'copied' next to quack-put
 function copied(clipboard) {
     // get reference to 'copied' element
     const copied = document.getElementById("copied");
@@ -32,8 +32,8 @@ function copied(clipboard) {
     // trigger animation
     copied.style.opacity = '1';
 
-    // set quark-put to clipboard
-    quark_put.innerHTML = clipboard;
+    // set quack-put to clipboard
+    quack_put.innerHTML = clipboard;
 
     // trigger fadeout animation
     window.setTimeout(function(){
